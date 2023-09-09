6004362e867d8899d78686ff3e8a92441faa9504
TheIronWolfModding <vleonavicius@hotmail.com>
2023-09-09 16:09:35 -0700

    WIP

--- src/dxvk/dxvk_options.cpp
+++ src/dxvk/dxvk_options.cpp
@@ -14,10 +14,5 @@ namespace dxvk {
     tearFree                      = config.getOption<Tristate>("dxvk.tearFree",               Tristate::Auto);
     forcedSampleRateShadingFactor = config.getOption<float>("dxvk.forceSampleRateShadingFactor",     1.0f);
     trackPipelineLifetime         = config.getOption<Tristate>("dxvk.trackPipelineLifetime",  Tristate::Auto);
-    useRawSsbo                    = config.getOption<Tristate>("dxvk.useRawSsbo",             Tristate::Auto);
-    maxChunkSize                  = config.getOption<int32_t> ("dxvk.maxChunkSize",           0);
-    hud                           = config.getOption<std::string>("dxvk.hud", "");
-    tearFree                      = config.getOption<Tristate>("dxvk.tearFree",               Tristate::Auto);
-    forcedSampleRateShadingFactor = config.getOption<float>("dxvk.forceSampleRateShadingFactor",     1.0f);
   }
 }
