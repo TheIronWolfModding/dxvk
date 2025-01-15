265805d270c6a6b77b27578fcb6002bd6ce4f6fd
Antti Keränen <detegr@rbx.email>
2025-01-15 11:20:49 +0200

    WIP

--- src/d3d9/d3d9_initializer.cpp
+++ src/d3d9/d3d9_initializer.cpp
@@ -132,7 +132,7 @@ namespace dxvk {
         pitch * blockCount.height,
         alignedPitch,
         alignedPitch * blockCount.height,
-        D3D9CommonTexture::GetImageTypeFromResourceType(pTexture->GetType()),
+        D3D9CommonTexture::GetImageTypeFromResourceType(pTexture->GetType(), pTexture->Desc()->ArraySize),
         mipExtent,
         pTexture->Desc()->ArraySize,
         formatInfo,
